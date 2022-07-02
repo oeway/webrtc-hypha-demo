4e3ccc1f1e8dd910e954970af971a6efa6a272c3
oeway <oeway007@gmail.com>
2022-07-02 05:32:48 +0200

    add webrtc server

---
+++ README.md
@@ -0,0 +1,25 @@
+# WebRTC demo for Hypha
+
+
+This is a demo repo for showing how to use Hypha as a singaling server, and stream video to the browser.
+
+
+## Installation
+```
+pip install aiortc numpy imjoy-rpc
+```
+
+## Start
+
+Start the server:
+```
+python server.py
+```
+
+Now start a dev server for serving the webpage:
+```
+python -m http.server 9000
+```
+
+
+
---
+++ server.py
@@ -0,0 +1,141 @@
+import argparse
+import asyncio
+import logging
+import os
+import uuid
+import fractions
+
+import numpy as np
+from av import VideoFrame
+from imjoy_rpc.hypha import connect_to_server
+
+from aiortc import MediaStreamTrack, RTCPeerConnection, RTCSessionDescription
+
+ROOT = os.path.dirname(__file__)
+
+logger = logging.getLogger("pc")
+pcs = set()
+
+
+class VideoTransformTrack(MediaStreamTrack):
+    """
+    A video stream track that transforms frames from an another track.
+    """
+
+    kind = "video"
+
+    def __init__(self, transform):
+        super().__init__()  # don't forget this!
+        self.transform = transform
+        self.count = 0
+
+    async def recv(self):
+        # frame = await self.track.recv()
+        img = np.random.randint(0, 155, (150, 300, 3)).astype('uint8')
+        new_frame = VideoFrame.from_ndarray(img, format="bgr24")
+        new_frame.pts = self.count # frame.pts
+        self.count+=1
+        new_frame.time_base = fractions.Fraction(1, 1000)
+        return new_frame
+
+async def offer(params, context=None):
+    offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
+
+    pc = RTCPeerConnection()
+    pc_id = "PeerConnection(%s)" % uuid.uuid4()
+    pcs.add(pc)
+
+    def log_info(msg, *args):
+        logger.info(pc_id + " " + msg, *args)
+
+    log_info("Created for offer")
+
+    @pc.on("datachannel")
+    def on_datachannel(channel):
+        @channel.on("message")
+        def on_message(message):
+            if isinstance(message, str) and message.startswith("ping"):
+                channel.send("pong" + message[4:])
+
+    @pc.on("connectionstatechange")
+    async def on_connectionstatechange():
+        log_info("Connection state is %s", pc.connectionState)
+        if pc.connectionState == "failed":
+            await pc.close()
+            pcs.discard(pc)
+
+    @pc.on("track")
+    def on_track(track):
+        log_info("Track %s received", track.kind)
+        pc.addTrack(
+            VideoTransformTrack(transform=params["video_transform"]
+            )
+        )
+        @track.on("ended")
+        async def on_ended():
+            log_info("Track %s ended", track.kind)
+
+    # handle offer
+    await pc.setRemoteDescription(offer)
+
+    # send answer
+    answer = await pc.createAnswer()
+    await pc.setLocalDescription(answer)
+
+    return {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}
+
+
+async def on_shutdown(app):
+    # close peer connections
+    coros = [pc.close() for pc in pcs]
+    await asyncio.gather(*coros)
+    pcs.clear()
+
+
+async def start_service(service_id, workspace=None, token=None):
+    client_id = service_id + "-client"
+    print(f"Starting service...")
+    api = await connect_to_server(
+        {
+            "client_id": client_id,
+            "server_url": "https://ai.imjoy.io/",
+            "workspace": workspace,
+            "token": token,
+        }
+    )
+    # print("Workspace: ", workspace, "Token:", await api.generate_token({"expires_in": 3600*24*100}))
+    await api.register_service(
+        {
+            "id": service_id,
+            "config": {
+                "visibility": "public",
+                "require_context": True,
+            },
+            "offer": offer,
+        }
+    )
+    print(
+        f"Service (client_id={client_id}) started successfully, available at https://ai.imjoy.io/{api.config.workspace}/services"
+    )
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="WebRTC demo for video streaming"
+    )
+    parser.add_argument("--verbose", "-v", action="count")
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    loop = asyncio.get_event_loop()
+    loop.create_task(start_service(
+        "aiortc-demo",
+        workspace=None,
+        token=None,
+    ))
+    loop.run_forever()
+
+    
